24818120dc35ecaa01dd990697a5ad763027ef80
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-16 19:49:38 +0530

    Created Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Container from "./components/container";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Container />
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      class="text-center text-dark fixed-bottom"
+      style={{ backgroundColor: "rgba(255, 255, 255, 0.8)" }}
+    >
+      <div
+        class="text-center p-3"
+        style={{ backgroundColor: "rgba(255, 255, 255, 0.2)" }}
+      >
+        Developed with 💖 by
+        <a class="text-dark pl-1" href="https://www.linkedin.com/in/bandepalli-surya/">
+          Bandepalli Surya
+        </a>{" "}
+        and{" "}
+        <a class="text-dark" href="https://www.linkedin.com/in/abhishekmanikandan">
+          Abhishek Manikandan
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
