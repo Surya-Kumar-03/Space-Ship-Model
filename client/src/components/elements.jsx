82fac07f4af8bc03f0c18ddec05014ad7d59d524
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-17 14:51:30 +0530

    Group Parameter Created

--- client/src/components/elements.jsx
+++ client/src/components/elements.jsx
@@ -3,6 +3,7 @@ import CryoSleep from "./cryosleep";
 import Age from "./age";
 import Vip from "./vip";
 import Expense from "./expense";
+import Group from "./groups";
 
 function Elements() {
   const [pageNumber, setPageNumber] = useState(1);
@@ -19,6 +20,13 @@ function Elements() {
         <Age></Age>
         <Vip></Vip>
         <Expense changePage={changePageNumber}></Expense>
+      </div>
+    );
+  } else if (pageNumber === 2) {
+    return (
+      <div style={{ margin: "1rem" }}>
+        <h1>Just a few more!</h1>
+        <Group></Group>
       </div>
     );
   }
---
+++ client/src/components/groups.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+
+const groups = [
+  { value: 1, label: "Group 1" },
+  { value: 2, label: "Group 2" },
+  { value: 3, label: "Group 3" },
+  { value: 4, label: "Group 4" },
+  { value: 5, label: "Group 5" },
+  { value: 6, label: "Group 6" },
+];
+function Group() {
+    //selectedGroup
+  const [selectedGroup, setSelectedGroup] = useState(1);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: "1rem",
+      }}
+    >
+      <p>Group:</p>
+      <FormControl>
+        <Select
+          style={{ marginLeft: "1rem" }}
+          value={selectedGroup}
+          onChange={(event) => setSelectedGroup(event.target.value)}
+        >
+          {groups.map((group) => (
+            <MenuItem key={group.value} value={group.value}>
+              {group.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default Group;
