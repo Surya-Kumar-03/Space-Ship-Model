2d0e4a5eb36149cb735d7bb66a15bf7f094d3106
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-22 14:42:29 +0530

    Bug Fixes

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/public", "/index.html"));
+app.get("*", function (req , res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 var ourCryoSleep = 0;
