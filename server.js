91acfd7ab164f87423c0525461a8cd5fd754577a
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-22 14:53:40 +0530

    Cyclic Optimisation

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 
 app.get("*", function (req , res) {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 var ourCryoSleep = 0;
